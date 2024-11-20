458e798b27b9f1ba312e507db112b5d82992c24b
Thakur Aditya <adityathakurbusiness@gmail.com>
2024-11-20 23:09:09 +0530

    HeroSection Completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,14 @@ import './App.css'
 import Navbar from './components/Navbar'
 import { Button } from './components/ui/button'
 import Login from './pages/login'
+import Herosection from './pages/student/Herosection'
 
 
 function App() {
   return (
     <>
       <Navbar />
+      <Herosection />
       <Login />
 
     </>
