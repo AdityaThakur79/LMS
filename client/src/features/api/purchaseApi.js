9a3f5613d6154e1ae76ccf56704f5af6d5abe39a
Aditya Thakur <121376593+AdityaThakur79@users.noreply.github.com>
2024-12-05 03:16:40 +0530

    Update purchaseApi.js

--- client/src/features/api/purchaseApi.js
+++ client/src/features/api/purchaseApi.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const COURSE_PURCHASE_API = "https://lms-taxj.onrender.com";
+const COURSE_PURCHASE_API = "https://lms-taxj.onrender.com/api/purchase";
 
 export const purchaseApi = createApi({
   reducerPath: "purchaseApi",
