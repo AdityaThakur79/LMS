b342f858cf599d9ee8a77bbd42b745fb4b9a8b7c
Thakur Aditya <adityathakurbusiness@gmail.com>
2024-12-05 02:52:13 +0530

    Ready for Deployment

--- server/server.js
+++ server/server.js
@@ -8,6 +8,8 @@ import mediaRoutes from "./routes/mediaRoutes.js";
 import purchaseCourseRoutes from "./routes/purchaseCourseRoutes.js";
 import progressCourseRoutes from "./routes/courseProgressRoutes.js";
 import cookieParser from "cookie-parser";
+import path from "path";
+import { fileURLToPath } from "url";
 
 // configing the dotenv file
 dotenv.config();
@@ -34,8 +36,17 @@ app.use("/api/media", mediaRoutes);
 app.use("/api/purchase", purchaseCourseRoutes);
 app.use("/api/progress", progressCourseRoutes);
 
-app.use("/", (req, res) => {
-  res.send("HI");
+// Get the current file path
+const __filename = fileURLToPath(import.meta.url);
+// Get the directory name
+const __dirname = path.dirname(__filename);
+
+// Serve static files from the assets folder
+app.use(express.static(path.join(__dirname, "./client/dist")));
+
+// Route to serve `index.html`
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/dist/index.html"));
 });
 
 const PORT = process.env.PORT || 8080;
